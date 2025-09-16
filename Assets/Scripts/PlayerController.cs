4b744d937b7b5a09c2dbe7a233dce96cf87a94df
Matthew009 <matthewm6775@nycstudents.net>
2025-09-16 12:33:19 -0400

    Added movement
    I can move left and right with the help of the get.axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
+{// hello world
 // all my variables
 private float speed = 20.0f;
 private float turnSpeed = 45.0f;
